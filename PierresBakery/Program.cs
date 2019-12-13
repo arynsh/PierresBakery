f18e51b806751129c098f0ddf77c64ae9b5c1069
Sharon Lee <sharonlee714@gmail.com>
2019-12-13 10:18:18 -0800

    add branching to return results

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-// using Bakery;
 
 namespace Bakery
 { 
@@ -13,38 +12,54 @@ namespace Bakery
             // Animal donkey2 = new Animal ("Don Nutella", "female", "Area 1", "donkey");
             // Animal Emu1 = new Animal ("Waffle", "female", "Area 2", "emu");
             
-            // List<Animal> Animals = new List<Animal>() { donkey1, donkey2, Emu1 };
+            List<int> bakedGoods = new List<int> {};
 
             Console.WriteLine("Welcome to Pierre's Bakery! Or as we like to say, Bienvenue! We have loaves of bread for $5 each and pastries for $2 each. We are running a Christmas special for the month of December where our loaves of bread are buy 2 get 1 free and our pastries are 3 for $5!");
             Console.WriteLine("Please enter the number of loaves you would like to purchase today: ['0' to skip to pastries]");
             string stringBread = Console.ReadLine();
             int numBread = int.Parse(stringBread);
-            Console.WriteLine("Please enter the number of pastries you would like to purchase today: ['0' to skip to checkout]");
-            string stringPastries = Console.ReadLine();
-            int numPastries = int.Parse(stringPastries);
-            
             if (numBread != 0)
             {
-                Animal.LookUpByTypeMethod(Animals);
+                bakedGoods.Add(numBread);
+                // Animal.LookUpByTypeMethod(Animals);
+                Console.WriteLine("And how many pastries would you like? '0' to skip to checkout]");
+                string stringPastries = Console.ReadLine();
+                int numPastries = int.Parse(stringPastries);
                 
-            } else 
-            {
-                Console.WriteLine("Would you like to search by area? ['Y' for yes, 'Enter' for no]");
-                string answer2 = Console.ReadLine();
-                if (answer2 == "Y" || answer2 == "y")
+                if (numPastries != 0)
                 {
-                    Animal.LookUpByAreaMethod(Animals);
+                    bakedGoods.Add(numPastries);
+                    checkout();
+                }
+                else 
+                {
+                    checkout();
+                }
+
+            } 
+            else 
+            {
+                bakedGoods.Add(0);
+                Console.WriteLine("How many pastries would you like? '0' to skip to checkout]");
+                string stringPastries = Console.ReadLine();
+                int numPastries = int.Parse(stringPastries);
+
+                if (numPastries != 0)
+                {
+                    bakedGoods.Add(numPastries);
+                    checkout();
                 } 
                 else
                 {
-                    Console.WriteLine("Would you like to exit? ['Y' for yes, 'Enter' for no]");
-                    string answer3 = Console.ReadLine();
-                    if (answer3 == "Y" || answer3 == "y")
+                    Console.WriteLine("It seems you have not added anything to your cart today. Would you like to buy bread or pastries? ['Y' for yes, 'Enter' for no]");
+                    string answer = Console.ReadLine();
+                    if (answer == "Y" || answer == "y")
                     {
-                        Console.WriteLine("See you next time!!");
-                    }
-                    else{
                         Main();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thank you for coming in! We hope to see you again soon.");
                     }
                 }
             }
